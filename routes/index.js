2cab6c788305353f6b82a5c0cd68576018fe321e
Abe Rajeev <abhijithgr@hotmail.com>
2018-12-03 16:18:14 -0500

    first commit

---
+++ routes/index.js
@@ -0,0 +1,80 @@
+var express = require('express');
+var router = express.Router();
+var mongo = require('mongodb').MongoClient;
+var objectId = require('mongodb').ObjectID;
+var assert = require('assert');
+var nodemailer = require('nodemailer');
+
+var url = 'mongodb://localhost:27017/test';
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index');
+});
+
+router.post('/insert', function(req, res, next) {
+  var item = {
+    email: req.body.email
+  };
+
+  mongo.connect(url, function(err, db) {
+    assert.equal(null, err);
+    db.collection('userData').insertOne(item, function(err, result) {
+      assert.equal(null, err);
+      console.log('Item inserted');
+      db.close();
+    });
+  });
+
+  res.redirect('/');
+});
+
+
+router.post('/send_email', (req, res) => {
+  const output = `
+    <p>You have a new contact request</p>
+    <h3>Contact Details</h3>
+    <ul>  
+      <li>Name: ${req.body.name}</li>
+      <li>Company: ${req.body.company}</li>
+      <li>Email: ${req.body.email}</li>
+      <li>Phone: ${req.body.phone}</li>
+    </ul>
+    <h3>Message</h3>
+    <p>${req.body.message}</p>
+  `;
+
+  // create reusable transporter object using the default SMTP transport
+  let transporter = nodemailer.createTransport({
+    host: 'smtp.gmail.com',
+    port: 465,
+    secure: true, // true for 465, false for other ports
+    auth: {
+        user: 'aberaj57@gmail.com', // generated ethereal user
+        pass: 'your_password'  // generated ethereal password
+    }
+  });
+
+  // setup email data with unicode symbols
+  let mailOptions = {
+      from: '"Nodemailer Contact" <aberaj57@gmail.com>', // sender address
+      to: 'aberaj57@gmail.com', // list of receivers
+      subject: 'Node Contact Request', // Subject line
+      text: 'Hello world?', // plain text body
+      html: output // html body
+  };
+
+  // send mail with defined transport object
+  transporter.sendMail(mailOptions, (error, info) => {
+      if (error) {
+          return console.log(error);
+      }
+      console.log('Message sent: %s', info.messageId);   
+      console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
+
+      res.render('index', {msg:'Email has been sent'});
+  });
+});
+
+
+module.exports = router;
